01b86a903d4af119a4db19d855a0d7d372a7f2bc
charmine618 <charmine.leu@stonybrook.edu>
2023-05-12 03:11:24 +0000

    updated python

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder='template')
+
 @app.route('/')
 def hello_world():
     return render_template('home.html')
 
-app.run(host='0.0.0.0', port=5000)
+#app.run(host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    app.run(debug=True)
